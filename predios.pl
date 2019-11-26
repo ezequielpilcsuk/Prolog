11692b03fbc63f5ec5a52deb714ae0cff3eaf7e0
Ezequiel Pilcsuk da Rosa <ezequielpilcsuk@gmail.com>
2019-11-26 15:23:00 -0300

    Need to figure it out why it's not working, works on semi complete problems

--- predios.pl
+++ predios.pl
@@ -44,72 +44,44 @@ problem(1,Mx,Lr,Ud) :-
           [0,2],
           [0,1]].
         
-linhaTeste(1,[3,2,4,5,1], [3,2]).
+linhaTeste(1,[3,2,4,5,1], [0,0]).
 
         % chamada para printar solução
         % problem(666, Rows),
         % sudoku(Rows),
         % maplist(labeling([ff]), Rows),
         % maplist(portray_clause, Rows).
+        % problem(0,X),sudoku(X),maplist(labeling([ff]),X),maplist(portray_clause,X).
 
 wolkenkratzer(Matrix,Lr,Ud) :- 
-    puzzleSize(X),
+    puzzleSize(X),maxValue(Y),S is Y-X+1,
     length(Matrix,X),maplist(same_length(Matrix), Matrix),
-    append(Matrix,Vs), Vs ins 1..X,
-    maplist(all_distinct,Matrix),
-    transpose(Matrix,MatrixT),maplist(all_distinct,MatrixT),
+    append(Matrix,Vs), Vs ins S..X,
     checkLines(Matrix,Lr),
+    transpose(Matrix,MatrixT),
     checkLines(MatrixT,Ud).
-
-
 
 nPredios([],0,_).
 nPredios([H|T],A,M) :- 
-    H > M,
+    H #> M,
     nPredios(T,A1,H),
-    A is A1+1,!.
+    A #= A1+1,!.
 nPredios([_|T],A,M) :- nPredios(T,A,M).
 
+checkLine(L,[0,0]):- all_distinct(L),!.
+checkLine(L,[C1,0]) :-
+    all_distinct(L),
+    nPredios(L,C1,0),!.
+checkLine(L,[0,C2]) :-
+    all_distinct(L),
+    reverse(L,Lr),
+    nPredios(Lr,C2,0),!.
 checkLine(L,[C1,C2]) :- 
+    all_distinct(L),
     nPredios(L,C1,0),
     reverse(L,Lr),
-    nPredios(Lr,C2,0).
+    nPredios(Lr,C2,0),!.
 
 % solution(1,Mx,Lr,Ud),checkLines(Mx,Lr),transpose(Mx,Mxt),checkLines(Mxt,Ud).
 checkLines([],[]).
 checkLines([Row|T1],[Cond|T2]) :-checkLine(Row,Cond), checkLines(T1,T2).
-
-label(Col, Row, N) :- format("(~w) ~w ~w num\n", [N,Col,Row]).
-label(Col, Row, _) :- format("~w ~w clear\n", [Col,Row]), false.
-
-% solution(1,P) :-
-%         P = [linha(1,[5,3,1,4,2],3),
-%              linha(4,[1,2,3,5,4],2),
-%              linha(3,[3,4,5,2,1],3),
-%              linha(2,[4,5,2,1,3],0),
-%              linha(0,[2,1,4,3,5],1),
-%              linha(0,[5,1,3,4,2],0),
-%              linha(3,[3,2,4,5,1],2),
-%              linha(3,[1,3,5,2,4],2),
-%              linha(0,[4,5,2,1,3],2),
-%              linha(0,[2,4,1,3,5],1)]. 
-% problem(1, P) :-
-%         P = [linha(1,[],3),
-%              linha(4,[],2),
-%              linha(3,[],3),
-%              linha(2,[],0),
-%              linha(0,[],1),
-%              linha(0,[],0),
-%              linha(3,[],2),
-%              linha(3,[],2),
-%              linha(0,[],2),
-%              linha(0,[],1)].
-
-% getList(linha(_,L,_),L).
-
-% getLists([],[]).
-% getLists([H|T],[L1|L2]) :- getList(H,L1),getLists(T,L2).
-
-% getX(linha(X,_,_),X).
-
-% getY(linha(_,_,Y),Y).
