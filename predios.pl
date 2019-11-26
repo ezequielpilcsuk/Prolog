871b1603e2d57be563e8fe11270b5173bcdffe00
Ezequiel Pilcsuk da Rosa <ezequielpilcsuk@gmail.com>
2019-11-26 10:21:15 -0300

    Implemented nPredios, checkLine, maybe we need to think this through

--- predios.pl
+++ predios.pl
@@ -6,14 +6,43 @@
 %     Garantir que [0..n] linhas == transpose [n..2n]
 %     Garantir que soma de cada linha seja igual ao necessário
 
-% wolkenkratzer(Rows) :-
-%     length(Rows, 5), maplist(same_length(Rows), Rows),
-%     append
+problem(1, [linha(1,[5,3,1,4,2],3),
+            linha(4,[1,2,3,5,4],2),
+            linha(3,[3,4,5,2,1],3),
+            linha(2,[4,5,2,1,3],0),
+            linha(0,[2,1,4,3,5],1),
+            linha(0,[5,1,3,4,2],0),
+            linha(3,[3,2,4,5,1],2),
+            linha(3,[1,3,5,2,4],2),
+            linha(0,[],2),
+            linha(0,[],1)]).
 
-%    3 [2,3,5,4,1] 3
 
-predios(_,0, _):-!.
-predios([H|T], N, M) :- 
+wolkenkratzer(Linhas) :- 
+    length(Linhas,X),
+    maplist(same_length(Linhas), Linhas).
 
 
-satisfies(E,L,D) :- predios(L,D), reverse(L,LR), predios(LR,E).
+
+
+getList(linha(_,L,_),L).
+
+getLists([],[]).
+getLists([H|T],[L1|L2]) :- getList(H,L1),getLists(T,L2).
+
+linhaTeste(1,linha(3, [3,2,4,5,1], 2)).
+linha(3, [3,2,4,5,1], 2).
+
+nPredios([],0,_).
+nPredios([H|T],A,M) :- 
+    H > M,
+    nPredios(T,A1,H),
+    A is A1+1,!.
+nPredios([_|T],A,M) :- nPredios(T,A,M).
+
+checkLine(linha(E,L,D)) :- 
+    nPredios(L,E,0),
+    reverse(L,Lr),
+    nPredios(Lr,D,0),
+    all_distinct(L).
+
