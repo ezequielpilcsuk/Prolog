2dc6c85e0b2887f8e430554f9b7d7617f6a7a149
Ezequiel Pilcsuk da Rosa <ezequielpilcsuk@gmail.com>
2019-11-27 11:44:06 -0300

    1 line away, added another example, added diagoneles function

--- predios.pl
+++ predios.pl
@@ -14,11 +14,11 @@ puzzleSize(5).
 maxValue(5).
 
 solution(1,Mx,Lr,Ud) :-
-    Mx = [[_,3,1,4,2],
-          [1,_,3,5,4],
-          [3,4,_,2,1],
-          [4,5,2,_,3],
-          [2,1,4,3,_]],
+    Mx = [[5,3,1,4,2],
+          [1,2,3,5,4],
+          [3,4,5,2,1],
+          [4,5,2,1,3],
+          [2,1,4,3,5]],
     Lr = [[1,3],
           [4,2],
           [3,3],
@@ -29,6 +29,23 @@ solution(1,Mx,Lr,Ud) :-
           [3,2],
           [0,2],
           [0,1]].
+
+solution(2,Mx,Lr,Ud) :-
+    Mx = [[_,1,5,4,_],
+          [2,5,4,3,1],
+          [5,3,2,1,4],
+          [4,2,1,5,3],
+          [_,4,3,2,_]],
+    Lr = [[0,3],
+          [0,4],
+          [1,2],
+          [0,0],
+          [3,0]],
+    Ud = [[2,0],
+          [0,0],
+          [0,3],
+          [0,0],
+          [3,0]].
 
 problem(1,Mx,Lr,Ud) :-
     Mx = [[5,3,1,4,2],
@@ -46,15 +63,35 @@ problem(1,Mx,Lr,Ud) :-
           [3,2],
           [0,2],
           [0,1]].
+
+problem(2,Mx,Lr,Ud) :-
+    Mx = [[_,_,_,_,_],
+          [_,_,_,_,_],
+          [_,_,_,_,_],
+          [_,_,_,_,_],
+          [_,_,_,_,_]],
+    Lr = [[0,3],
+          [0,4],
+          [1,2],
+          [0,0],
+          [3,0]],
+    Ud = [[2,0],
+          [0,0],
+          [0,3],
+          [0,0],
+          [3,0]].
+
+        
+
         
 linhaTeste(1,[3,2,4,5,1], [0,0]).
 
-        % chamada para printar solução
-        % problem(666, Rows),
-        % sudoku(Rows),
-        % maplist(labeling([ff]), Rows),
-        % maplist(portray_clause, Rows).
-        % problem(0,X),sudoku(X),maplist(labeling([ff]),X),maplist(portray_clause,X).
+        % chamada para printar solução, funciona se não é necessário chamar 
+        % solution(1, M,X,Y),
+        % wolkenkratzer(M,X,Y),
+        % maplist(labeling([leftmost]), M),
+        % maplist(portray_clause, M).
+        
 
 wolkenkratzer(Matrix,Lr,Ud) :- 
     puzzleSize(X),maxValue(Y),S is Y-X+1,
@@ -63,7 +100,12 @@ wolkenkratzer(Matrix,Lr,Ud) :-
     checkLines(Matrix,Lr),
     transpose(Matrix,MatrixT),
     checkLines(MatrixT,Ud).
+    % jogo original não preve restrições às diagonais
+    % diagonales(Matrix,[D1,D2]),
+    % all_distinct(D1),
+    % all_distinct(D2).
 
+nPredios(L,R) :- nPredios(L,R,0).
 nPredios([],0,_).
 nPredios([H|T],A,M) :- 
     H #> M,
@@ -74,17 +116,21 @@ nPredios([_|T],A,M) :- nPredios(T,A,M).
 checkLine(L,[0,0]):- all_distinct(L),!.
 checkLine(L,[C1,0]) :-
     all_distinct(L),
-    nPredios(L,C1,0),!.
+    nPredios(L,C1),!.
 checkLine(L,[0,C2]) :-
     all_distinct(L),
     reverse(L,Lr),
-    nPredios(Lr,C2,0),!.
+    nPredios(Lr,C2),!.
 checkLine(L,[C1,C2]) :- 
     all_distinct(L),
-    nPredios(L,C1,0),
+    nPredios(L,C1),
     reverse(L,Lr),
-    nPredios(Lr,C2,0),!.
+    nPredios(Lr,C2),!.
 
-% solution(1,Mx,Lr,Ud),checkLines(Mx,Lr),transpose(Mx,Mxt),checkLines(Mxt,Ud).
 checkLines([],[]).
 checkLines([Row|T1],[Cond|T2]) :-checkLine(Row,Cond), checkLines(T1,T2).
+
+diagonales(Matrix, [L1, L2]) :-
+    length(Matrix, N),
+    findall(E, (between(1,N,I), nth1(I, Matrix, Row), nth1(I, Row, E)), L1),
+    findall(E, (between(1,N,I), J is N+1-I, nth1(I, Matrix, Row), nth1(J,Row,E)),L2).
