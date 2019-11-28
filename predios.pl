3c362f44c3b6a5cc6024116b794eaf982bb65fa5
Ezequiel Pilcsuk da Rosa <ezequielpilcsuk@gmail.com>
2019-11-28 10:32:32 -0200

    Corrected a bug on checkLine, need to check if all latin squares are hapenning, probably not

--- predios.pl
+++ predios.pl
@@ -9,7 +9,7 @@
 % Descobri que ele completa o jogo quando ele só encontra uma forma de preencher corretamente.
 % necessário, de alguma forma, caso encontre false, testar outro possivel m
 
-% game(Matrix,LeftRight,UpDown).
+% game(Matrix,LeftRightRestrict,UpDownRestrict).
 puzzleSize(5).
 maxValue(5).
 
@@ -32,9 +32,9 @@ solution(1,Mx,Lr,Ud) :-
 
 solution(2,Mx,Lr,Ud) :-
     Mx = [[_,1,5,4,_],
-          [2,5,4,3,1],
-          [5,3,2,1,4],
-          [4,2,1,5,3],
+          [_,5,4,3,1],
+          [_,3,2,1,4],
+          [_,2,1,5,3],
           [_,4,3,2,_]],
     Lr = [[0,3],
           [0,4],
@@ -48,11 +48,11 @@ solution(2,Mx,Lr,Ud) :-
           [3,0]].
 
 problem(1,Mx,Lr,Ud) :-
-    Mx = [[5,3,1,4,2],
+    Mx = [[_,_,_,_,_],
           [_,_,_,_,_],
-          [3,4,5,2,1],
-          [_,_,2,1,3],
-          [2,1,4,3,5]],
+          [_,_,_,_,_],
+          [_,_,_,_,_],
+          [_,_,_,_,_]],
     Lr = [[1,3],
           [4,2],
           [3,3],
@@ -94,43 +94,48 @@ linhaTeste(1,[3,2,4,5,1], [0,0]).
         
 
 wolkenkratzer(Matrix,Lr,Ud) :- 
-    puzzleSize(X),maxValue(Y),S is Y-X+1,
-    length(Matrix,X),maplist(same_length(Matrix), Matrix),
-    append(Matrix,Vs), Vs ins S..X,
-    checkLines(Matrix,Lr),
-    transpose(Matrix,MatrixT),
-    checkLines(MatrixT,Ud).
+      puzzleSize(X),maxValue(Y),S is Y-X+1,
+      length(Matrix,X),maplist(same_length(Matrix), Matrix),
+      append(Matrix,Vs), Vs ins S..Y,
+      maplist(all_distinct,Matrix),
+      transpose(Matrix,MatrixT),maplist(all_distinct,MatrixT).
+      % checkLines(Matrix,Lr).
+    % transpose(Matrix,MatrixT),checkLines(MatrixT,Ud).
     % jogo original não preve restrições às diagonais
     % diagonales(Matrix,[D1,D2]),
     % all_distinct(D1),
     % all_distinct(D2).
 
-nPredios(L,R) :- nPredios(L,R,0).
-nPredios([],0,_).
+nPredios(L,R) :- nPredios(L,R,0),!.
+nPredios([],0,_):-!.
 nPredios([H|T],A,M) :- 
     H #> M,
     nPredios(T,A1,H),
     A #= A1+1,!.
-nPredios([_|T],A,M) :- nPredios(T,A,M).
+nPredios([_|T],A,M) :- nPredios(T,A,M),!.
 
 checkLine(L,[0,0]):- all_distinct(L),!.
 checkLine(L,[C1,0]) :-
     all_distinct(L),
-    nPredios(L,C1),!.
+    nPredios(L,X),
+    X == C1,!.
 checkLine(L,[0,C2]) :-
     all_distinct(L),
     reverse(L,Lr),
-    nPredios(Lr,C2),!.
+    nPredios(Lr,X),
+    X == C2,!.
 checkLine(L,[C1,C2]) :- 
     all_distinct(L),
-    nPredios(L,C1),
+    nPredios(L,X),
+    X == C1,
     reverse(L,Lr),
-    nPredios(Lr,C2),!.
+    nPredios(Lr,Y),
+    Y == C2,!.
 
 checkLines([],[]).
 checkLines([Row|T1],[Cond|T2]) :-checkLine(Row,Cond), checkLines(T1,T2).
 
-diagonales(Matrix, [L1, L2]) :-
+diagonals(Matrix, [L1, L2]) :-
     length(Matrix, N),
     findall(E, (between(1,N,I), nth1(I, Matrix, Row), nth1(I, Row, E)), L1),
     findall(E, (between(1,N,I), J is N+1-I, nth1(I, Matrix, Row), nth1(J,Row,E)),L2).
